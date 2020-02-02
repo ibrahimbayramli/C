d8d22daf5f09bd6303a9a0b70f8a8bda4829b3f0
ibrahim <ibrahimbayramli1902@gmail.com>
2020-02-03 00:39:11 +0300

    Create sayma.c

---
+++ sayma.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/*
+   Ýbrahim BAYRAMLI tarafýndan yazýlmýþtýr
+   https://sites.google.com/view/ibrahim-bayramli
+   
+   1 den 25 e kadar sayýp sonra geri geri sayan kod
+*/
+
+int main(){
+	
+	int i;
+	
+	for(i=1;i<=25;i=i+1){
+		
+			
+		printf("%d\n",i);
+		
+	}
+	
+	for(i=25;i>0;i=i-1){
+		
+		
+		printf("\t%d\n",i);
+		
+		
+		
+	}
+	
+	return 0;
+	
+}
